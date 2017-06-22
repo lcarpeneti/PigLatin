d1cb97f1f1bbd6fbcf8323bf0a1549b6e41e2eb0
Iroveena Pagsibigan and Lia Carpeneti <iroveena@>
2017-06-21 21:00:10 -0700

    add language for project

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ var pigLatin = function(word) {
   if (!word.match(/^[a-zA-Z]+$/)) {
     return "not a word";
   } else if (word[0].startsWith("a") || word[0].startsWith("e") || word[0].startsWith("i") || word[0].startsWith("o") || word[0].startsWith("u")) {
-     return "hello";
+     return ;
   } else {
       return "nothing";
     }
@@ -27,3 +27,17 @@ $(document).ready(function() {
     $("#result").show();
   });
 });
+
+
+ var input = $("").val();
+for (i=1;i<=input, i++){
+  if (i % 3 = 0) {
+    return "ping";
+  } else if (i % 5) {
+    return "pong";
+  { else if (i % 15){
+    return "pingpong";
+  } else return "i";
+
+  $(".spot")append(i+"<br>");
+}
