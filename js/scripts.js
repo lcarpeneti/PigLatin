7615c30fb04f24d11dbcd0c4056c3227d5e2fde0
Iroveena Pagsibigan and Lia Carpeneti <iroveena@>
2017-06-21 20:41:27 -0700

    add additional business logic to identify the first letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //business logic
 var pigLatin = function(word) {
+  //console.log(arrayOfWord);
   //return word;
   if (!word.match(/^[a-zA-Z]+$/)) {
     return "not a word";
+  } else if (word[0].startsWith("a") || word[0].startsWith("e") || word[0].startsWith("i") || word[0].startsWith("o") || word[0].startsWith("u")) {
+     return "hello";
   } else {
-    return word;
-  }
-};
+      return "nothing";
+    }
+}
 
 
 
@@ -15,6 +18,8 @@ $(document).ready(function() {
   $("form#pig-latin").submit(function(event) {
     event.preventDefault();
     var word = ($("input#word").val());
+    var arrayOfWord = word.split(" ");
+    //console.log(arrayOfWord);
     var answer = pigLatin(word);
 
     $(".word").text(word);
